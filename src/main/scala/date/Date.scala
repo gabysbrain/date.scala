aac8ba5700b38f55cd1da964e7c31d19dd50c9cb
Tom Torsney-Weir <tom@katana.macs.dnsdojo.net>
2011-07-13 00:12:45 -0700

    date parser getting done

--- src/main/scala/date/Date.scala
+++ src/main/scala/date/Date.scala
@@ -1,6 +1,7 @@
 package date.scala
 
 import date.scala.grammar.Timespan
+import com.eaio.util.text.HumanTime
 import scala.util.parsing.combinator._
 
 object Date extends Timespan {
@@ -9,5 +10,10 @@ object Date extends Timespan {
     println(parseAll(value, args(0)))
     //val fst :: _ = parseAll(value, args(0))
   }
+
+  def string2Millis(s:String) : Long = parseAll(value, s)
+
+  def millis2Exact(m:Long) : String = HumanTime.exactly(m)
+  def millis2Approx(m:Long) : String = HumanTime.approximately(m)
 }
 
